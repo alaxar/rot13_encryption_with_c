8384fe5f45ec3c695362396c3ea59a2434c4e172
alaxar <alazardemessie17@gmail.com>
2021-05-13 01:33:13 +0300

    Removing unused variables

--- rot13.c
+++ rot13.c
@@ -6,7 +6,6 @@ char *rot13_encrypt(char letter, int iteration_value);
 
 
 // global variables
-char all_cases[52];
 char alphabets[26];
 char *cipher = NULL;
 int index_pos;
@@ -14,14 +13,13 @@ int index_pos;
 int main(int argc, char *argv[]) {
 	int cipher_size = 0;
 
-	if(argc == 2) {
+	if(argc == 2) { // checking if the user has provided a message to either encrypt or decrypt.
 
 		// loading alphabets to a char array
 		for(int i = 0, j = 0; i < 26; i++) {
 			alphabets[i] = (char)97 + i;
 		}
 
-		printf("%c", all_cases[1 + 26]);
 		// determine the size of the input characters
 		for(int i = 0; argv[1][i] != '\0'; i++) {
 			cipher_size++;
@@ -30,12 +28,12 @@ int main(int argc, char *argv[]) {
 		// allocate memory for cipher pointer variable by the size of the user input value
 		cipher = (char*)malloc(cipher_size);
 
-		// check if the letter is whether its uppercase or lowercase
+		// parsing into user input character and sending it to rot13_encrpt function
 		for(int i = 0; argv[1][i] != '\0'; i++) {
 			rot13_encrypt(argv[1][i], i);
 		}
-		printf("Original text: %s\n", argv[1]);
-		printf("Cipher text: %s\n", cipher);
+		fprintf(stdout, "Original text: %s\n", argv[1]);
+		fprintf(stdout, "Cipher text: %s\n", cipher);
 	} else {
 		fprintf(stdout, "Usage: %s <message_to_either_encrypt_or_decrypt>\n", argv[0]);
 	}
@@ -43,8 +41,9 @@ int main(int argc, char *argv[]) {
 }
 
 char *rot13_encrypt(char letter, int iteration_value) {
-	index_pos = letter - 97;
-	int rotation_key = 13;
+	index_pos = letter - 97;	// get the index value by subtracting 97 from the letter which the user provided, 97 is for lowercase of a
+	
+	int rotation_key = 13;		// rotation key
 	cipher[iteration_value] =  alphabets[(index_pos + rotation_key) % 26];
 	return cipher;
 }
